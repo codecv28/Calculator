0a614cb5c9ec5e6f1d8e13c654a4a4cebe6c83b8
Chetan Verma <coutcv28@gmail.com>
2025-05-19 16:31:54 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,14 @@
+// const btn = document.querySelector('.btn');
+// const display = document.querySelector('.displayPanel');
+
+// btn.addEventListener('click' , () => {
+//     const value = document.querySelector('.input').value;
+//     if(!value){
+//         alert("Please enter something");
+//     }
+//     else{
+
+//         display.innerHTML = `<p>${value}</p>`;
+//     }
+// })
+
