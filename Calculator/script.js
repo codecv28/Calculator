1a12bc9a5917a6bc013aa8b45e1d72cdf95a0d69
Chetan Verma <coutcv28@gmail.com>
2025-05-21 00:11:16 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,6 +4,93 @@ const input = document.querySelector('.input');
 function append(char) {
     input.value += char;
 }
+const π = Math.PI;
+const e = Math.E;
+function specialAppend(char){
+        switch (char){
+            case 'π':
+                input.value += π;
+                break;
+            case 'e':
+                input.value += e;
+                break;
+            
+        }
+    }
+
+function factorial() {
+    const z = parseFloat(input.value);
+    if (isFinite(z) && z >= 0) {
+        const result = math.gamma(z + 1); // factorial(n) = Gamma(n + 1)
+        input.value = result.toFixed(6);  // round to 6 decimal places
+    } else {
+        input.value = 'Error';
+    }
+}
+
+function absolute(){
+    if(isFinite(input.value)){
+        if(input.value>=0){
+            return input.value;
+        }
+        else{
+            input.value = input.value*-1;
+        }
+    }
+    else{
+        input.value = 'Error';
+    }
+}
+ 
+function xtoPowery(){
+    if(isFinite(input.value)){
+        
+        input.value += '**';
+    }
+    else{
+        input.value = 'Error'
+    }
+}
+
+// console.log(math.log(10))
+// console.log(Math.log(10,1 0));
+// // // // function logarithm(){
+// // // //     if(isFinite(input.value)){
+// // // //         input.value += 'log base';
+// // // //     }
+// // // // }
+function lnLOG(){
+    if(isFinite(input.value)){
+        input.value = Math.log(input.value);
+    }
+    else{
+        input.value = 'Error'
+    }
+}
+function tenLOG(){
+    if(isFinite(input.value)){
+        input.value = Math.log10(input.value);
+    }
+    else{
+        input.value = 'Error'
+    }
+}
+function etoPowery(){
+    input.value += 'e**' ;
+}
+function tentoPowerx(){
+    input.value += '10**';
+}
+function xRooty(){
+    if(isFinite(input.value)){
+        
+        input.value += 'yroot';
+    }
+    else{
+        input.value = 'Error'
+    }
+}
+
 function blank() {
     input.value = '';
 }
@@ -41,20 +128,172 @@ function sqroot() {
 
 function sign() {
     if (isFinite(input.value)) {
+        input.value = parseFloat(input.value);
         input.value = input.value * (-1)
     }
     else { input.value = 'Error' }
 }
 
-function solve() {
-    try {
-
-        input.value = eval(input.value)
+function roundOF(num){
+    if((num < 0.000001 && num>0)||(num<0 && num>-0.000001)){
+        num = 0;
+        return num;
     }
-    catch {
-        input.value = 'Error'
+    else if(num > 0.99999 && num<1){
+        num = 1;
+        return num;
+    }
+    else if(num>-1 && num<-0.99999){
+        num=-1;
+        return num;
+    }
+    else{
+        return num;
     }
 }
+
+function trigoperator(char){
+    if(isFinite(input.value)){
+
+        let angle = input.value*Math.PI/180;
+        let temp;
+
+        switch (char){
+            case 'sin':
+                temp = Math.sin(angle);
+                input.value = roundOF(temp);
+                break;
+            case 'cos':
+                temp = Math.cos(angle);
+                input.value = roundOF(temp);
+                break;
+            case 'tan':
+                if(angle% (Math.PI/2) == 0 && angle%2 != 0){
+                    input.value = 'Undefined';
+                }
+                else{
+
+                    temp = Math.tan(angle);
+                    input.value = roundOF(temp);
+                }
+                    break;
+            case 'cosec':
+                if(angle % Math.PI == 0){
+                    input.value = 'Undefined';
+                }
+                else{
+
+                    temp = 1/Math.sin(angle);
+                    input.value = roundOF(temp);
+                }
+                    break;
+            case 'sec':
+                if(angle% (Math.PI/2) == 0 && angle%2 != 0){
+                    input.value = 'Undefined';
+                }
+                else{
+
+                    temp = 1/Math.cos(angle);
+                    input.value = roundOF(temp);
+                }
+                break;
+            case 'cot':
+                if(angle % Math.PI == 0){
+                    input.value = 'Undefined';
+                }
+                else{
+                temp = 1/Math.tan(angle);
+                input.value = roundOF(temp);
+                }
+                break;
+            default:
+                input.value = 'Error'
+        }
+    }
+    else{
+        input.value = 'Error';
+    }
+}
+
+function inverseTrig1(char){
+    if(isFinite(input.value)){
+        let z = input.value;
+        if(z>=-1 && z<=1){
+            switch (char){
+                case 'sin⁻¹' :
+                    input.value = Math.asin(z)*180/Math.PI;
+                    break;
+                case 'cos⁻¹' :
+                    input.value = Math.acos(z)*180/Math.PI;
+                    break;
+                default:
+                    input.value = 'Error';
+            }
+        }
+        else if(z>=1 && z<=-1){
+            switch (char){
+                case 'cosec⁻¹' :
+                    input.value = Math.asin(1/z)*180/Math.PI;
+                    break;
+                case 'sec⁻¹' :
+                    input.value = Math.acos(1/z)*180/Math.PI;
+                    break;
+                default:
+                    input.value = 'Error';
+            }
+        }
+        // else{
+        //     switch (char){
+        //         case 'tan⁻¹' :
+        //             input.value = Math.atan(z)*180/Math.PI;
+        //             break;
+        //         case 'cot⁻¹' :
+        //             input.value = Math.acos(1/z)*180/Math.PI;
+        //             break;
+        //         default:
+        //             input.value = 'Error';
+        //     }
+        // }
+    }
+    else{
+        input.value = 'Error';
+    }
+}
+
+function inverseTrig2(char){
+    if(isFinite(input.value)){
+        let z = input.value;
+
+            switch (char){
+                case 'tan⁻¹' :
+                    input.value = Math.atan(z)*180/Math.PI;
+                    break;
+                case 'cot⁻¹' :
+                    input.value = Math.acos(1/z)*180/Math.PI;
+                    break;
+                default:
+                    input.value = 'Error';
+            }
+
+    }
+    else{
+        input.value = 'Error';
+    }
+} 
+
+function solve() {
+    if (!input.value.trim()) {
+        input.value = '';
+        return;
+    }
+
+    try {
+        input.value = eval(input.value);
+    } catch {
+        input.value = 'Error';
+    }
+}
+
 
 //sidebar functionality
 const sidebarBTN = document.querySelector('.sidebarBTN');
@@ -84,8 +323,8 @@ const scientificSec = document.querySelector('.scientificSec');
 
 const title = document.querySelector('.title');
 
-Standard.addEventListener('click' , () => {
-    
+Standard.addEventListener('click', () => {
+
     scientificSec.style.display = 'none';
     standardSec.style.display = 'block';
     sideSection.style.display == 'none';
@@ -94,10 +333,10 @@ Standard.addEventListener('click' , () => {
     sideSection.classList.remove("scisideSection");
     title.innerHTML = `Standard Calculator`;
 })
-  
 
-Scientific.addEventListener('click' , () => {
-    
+
+Scientific.addEventListener('click', () => {
+
     standardSec.style.display = 'none';
     scientificSec.style.display = 'block';
     input.classList.add("sciInput");
